04f9fc478786196523a157ace8bb1cabaf6d2ea3
Sres0 <susanarepo@gmail.com>
2021-09-07 14:14:08 -0500

    hand trackers + fps

--- handTracking.py
+++ handTracking.py
@@ -6,14 +6,29 @@ cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
 
 while True:
     success, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    processedHands = hands.process(rgb)
+    # print(processedHands.multi_hand_landmarks)
+    if processedHands.multi_hand_landmarks:
+        for landmarks in processedHands.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, landmarks, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 255, 255), 2)
 
     cv2.imshow('Image', img)
-    cv2.imshow('B&W', gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # cv2.imshow('RGB', rgb)
+    if cv2.waitKey(1) & 0xFF == ord('q'): #waitKey(0)
         break
 
 cv2.destroyAllWindows()
