e7237cf63f45a18cc91f638522e994830939f5c9
Sres0 <susanarepo@gmail.com>
2021-09-07 15:14:00 -0500

    hand tracking fn

--- handTracking.py
+++ handTracking.py
@@ -2,41 +2,46 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture(0)
+class HandDetection():
+    def __init__(self, mode = False, maxHands = 2, detectionCon = 0.5, trackCon = 0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
 
-pTime = 0
-cTime = 0
+    def findHands(self, img, draw = True):
+        rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        processedHands = self.hands.process(rgb)
+        if processedHands.multi_hand_landmarks:
+            if draw:
+                for landmarks in processedHands.multi_hand_landmarks:
+                    self.mpDraw.draw_landmarks(img, landmarks, self.mpHands.HAND_CONNECTIONS)
+        return img
 
-while True:
-    success, img = cap.read()
-    rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    processedHands = hands.process(rgb)
-    # print(processedHands.multi_hand_landmarks)
-    if processedHands.multi_hand_landmarks:
-        for landmarks in processedHands.multi_hand_landmarks:
-            for id, lm in enumerate(landmarks.landmark):
-                # print(id, lm)
-                h, w, c = img.shape #height, width, channels
-                cx, cy = int(lm.x*w), int(lm.y*h) #position for pixel values
-                print(id, cx, cy)
-                if id == 0:
-                    cv2.circle(img, (cx, cy), 10, (255, 255, 255), 2, cv2.FILLED)
-            mpDraw.draw_landmarks(img, landmarks, mpHands.HAND_CONNECTIONS)
+def handTracking():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = HandDetection()
 
-    cTime = time.time()
-    fps = 1/(cTime - pTime)
-    pTime = cTime
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
 
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 255, 255), 2)
+        cTime = time.time()
+        fps = 1/(cTime - pTime)
+        pTime = cTime
 
-    cv2.imshow('Image', img)
-    # cv2.imshow('RGB', rgb)
-    if cv2.waitKey(1) & 0xFF == ord('q'): #waitKey(0)
-        break
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 255, 255), 2)
+        cv2.imshow('Image', img)
+        if cv2.waitKey(1) & 0xFF == ord('q'): #waitKey(0)
+            break
 
-cv2.destroyAllWindows()
-cap.release()
+    cv2.destroyAllWindows()
+    cap.release()
+
+handTracking()
