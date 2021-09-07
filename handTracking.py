10564fb22b84bc1e633f72e2378d43b82c93f43f
Sres0 <susanarepo@gmail.com>
2021-09-07 16:22:03 -0500

    + find position

--- handTracking.py
+++ handTracking.py
@@ -15,12 +15,24 @@ class HandDetection():
 
     def findHands(self, img, draw = True):
         rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        processedHands = self.hands.process(rgb)
-        if processedHands.multi_hand_landmarks:
+        self.processedHands = self.hands.process(rgb)
+        if self.processedHands.multi_hand_landmarks:
             if draw:
-                for landmarks in processedHands.multi_hand_landmarks:
+                for landmarks in self.processedHands.multi_hand_landmarks:
                     self.mpDraw.draw_landmarks(img, landmarks, self.mpHands.HAND_CONNECTIONS)
         return img
+
+    def findPosition(self, img, handNum = 0, draw = True): #only one hand
+        lmList = []
+        if self.processedHands.multi_hand_landmarks:
+            hand = self.processedHands.multi_hand_landmarks[handNum]
+            for id, lm in enumerate(hand.landmark):
+                h, w, _ = img.shape
+                x, y = int(lm.x*w), int(lm.y*h)
+                lmList.append([id, x, y])
+                if draw:
+                    cv2.circle(img, (x, y), 10, (255, 255, 255), cv2.FILLED)
+        return lmList
 
 def handTracking():
     pTime = 0
@@ -29,8 +41,11 @@ def handTracking():
     detector = HandDetection()
 
     while True:
-        success, img = cap.read()
+        _, img = cap.read()
         img = detector.findHands(img)
+        lmList = detector.findPosition(img, draw = False)
+        if len(lmList) != 0:
+            print(lmList[12]) # index tip positions
 
         cTime = time.time()
         fps = 1/(cTime - pTime)
@@ -44,4 +59,5 @@ def handTracking():
     cv2.destroyAllWindows()
     cap.release()
 
-handTracking()
+if __name__ == '__main__':
+    handTracking()
