83e23fd160e265d91c0d1d4355ad44cdc8f18ef5
Sres0 <susanarepo@gmail.com>
2021-11-16 08:40:20 -0500

    - pre & post

--- Processing/Pre/Tracking/blur.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-
-cap = cv.VideoCapture(0)
-
-while True:
-    _, frame = cap.read()
-    cv.imshow('UV', frame)
-    if cv.waitKey(2) & 0xFF == ord('q'): #waitKey(0)
-        break
