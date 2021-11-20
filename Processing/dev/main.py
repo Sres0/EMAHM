8c8fc2b9e0dadd4036766b41d76e5cd874044405
Sres0 <susanarepo@gmail.com>
2021-11-20 00:43:02 -0500

    functional find threshold in histogram. bug: only white hands

--- Processing/dev/main.py
+++ Processing/dev/main.py
@@ -1,8 +1,9 @@
 import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.signal as sig
 
-i = 73 # con gel
+i = 81 # con gel
 j = i - 2
 
 def path(i):
@@ -22,7 +23,7 @@ imgGel = createImg(i, 1)
 imgNoGel = createImg(j, 1)
 # cv.imshow('Segmented gel & no gel', np.hstack([imgGel, imgNoGel]))
 
-### BLUR ###
+### Mask ###
 
 medium_blur = (3,3)
 
@@ -31,35 +32,64 @@ blurredImgNoGel = cv.blur(imgNoGel, medium_blur)
 grayBlurredImgGel = cv.blur(cv.cvtColor(blurredImgGel, cv.COLOR_BGR2GRAY), medium_blur)
 grayBlurredImgNoGel = cv.blur(cv.cvtColor(blurredImgNoGel, cv.COLOR_BGR2GRAY), medium_blur)
 
-def one_d_histogram(img, title):
-    lColors = ('b', 'g', 'r')
-    plt.figure()
-    plt.title(title)
-    plt.xlabel('Bins')
-    plt.ylabel('Pixels')
-    plt.grid()
+def one_d_histogram(img, title, hide=False):
+    if not hide:
+        plt.figure()
+        plt.title(title)
+        plt.xlabel('Bins')
+        plt.ylabel('Pixels')
+        plt.grid()
+        plt.xlim([0, 256])
 
     if type(img) == list:
-        for (chan, col) in zip(img, lColors):
+        for chan in img:
             hist = cv.calcHist([chan], [0], None, [255], [0, 256])
-            plt.plot(hist, color=col)
     else:
         hist = cv.calcHist([img], [0], None, [255], [0, 256])
-        plt.plot(hist)
-    plt.xlim([0, 256])
+    if not hide: plt.plot(hist)
+    hist = [y[0] for y in hist]
+    return hist
+
+def find_hand_thresh(hist, fs=60, hide=True, title='hand threshold'):
+
+    # outliers
+    y = np.where(hist > (np.mean(hist) + 10*np.std(hist)), np.mean(hist), hist)
+    y = np.where(hist < (np.mean(hist) - 10*np.std(hist)), np.mean(hist), hist)
+
+    # butter
+    wn = 8/(fs/2)
+    b, a = sig.butter(3, wn, btype='lowpass')
+    y = sig.filtfilt(b, a, y)
+    x = np.array(range(0,255))
+
+    # peaks
+    min_peaks, _ = sig.find_peaks(-y, height=(-500, -1))
+    thresh = next(filter(lambda index: index > 50, min_peaks), None)
+    if not hide:
+        plt.figure()
+        plt.title(title + f' | {thresh}')
+        plt.grid()
+        plt.plot(x,hist)
+        plt.plot(x,y, color='red')
+        plt.plot(thresh,y[thresh], "x")
+        plt.xlim([0, 256])
+    return thresh
 
 # one_d_histogram(blurredImgGel, 'Blurred gel')
-one_d_histogram(grayBlurredImgGel, 'Blurred gray gel')
-one_d_histogram(grayBlurredImgNoGel, 'Blurred gray no gel')
+hGrayBlurredImgGel = one_d_histogram(grayBlurredImgGel, 'Blurred gray gel', hide=True)
+hGrayBlurredImgNoGel = one_d_histogram(grayBlurredImgNoGel, 'Blurred gray no gel', hide=True)
 
 def threshold(i, mval, img, channel_name, method=cv.THRESH_BINARY, hide=False):
     _, thresh = cv.threshold(img, mval, 255, method)
     if not hide: cv.imshow(f'Mascara {channel_name} {i} | {mval}', thresh)
     return thresh
 
-mGrayBlurredImgGel = threshold(i, 81.5, grayBlurredImgGel, 'gray blurqred gel', hide=True)
-mGrayBlurredImgNoGel = threshold(i, 81.5, grayBlurredImgNoGel, 'gray blurred no gel', hide=False)
+mGrayBlurredImgGel = threshold(i, find_hand_thresh(hGrayBlurredImgGel, title='gel thresh', hide=False), grayBlurredImgGel, 'gray blurqred gel', hide=True)
+mGrayBlurredImgNoGel = threshold(i, find_hand_thresh(hGrayBlurredImgNoGel, title='no gel thresh', hide=False), grayBlurredImgNoGel, 'gray blurred no gel', hide=True)
 # cv.imshow('Segmented gel & no gel', np.hstack([mGrayBlurredImgGel, mGrayBlurredImgNoGel]))
+
+mGrayBlurredImgGel = cv.erode(mGrayBlurredImgGel, np.array((2,2), dtype='uint8'), iterations=1)
+mGrayBlurredImgNoGel = cv.erode(mGrayBlurredImgNoGel, np.array((2,2), dtype='uint8'), iterations=1)
 
 def contour(mask, img, color, i, title, hide=False):
     contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
