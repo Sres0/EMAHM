ba8a593dc8b6b2b0dc1bd9d2fb8a829e025085a5
Sres0 <susanarepo@gmail.com>
2021-11-26 09:56:24 -0500

    hsv contour trackbar

--- Processing/dev/main.py
+++ Processing/dev/main.py
@@ -104,7 +104,8 @@ def contour(mask, img, color, i, title, hide=True):
     for cnt in contours:
         cv.drawContours(img, [cnt], -1, color, 1)
         cv.drawContours(blank, [cnt], -1, color, -1)
-    if not hide: cv.imshow(f'contorno {title} {i} y mascara', np.hstack([img, blank]))
+    # if not hide: cv.imshow(f'contorno {title} {i} y mascara', np.hstack([img, blank]))
+    if not hide: cv.imshow(title, np.hstack([img, blank]))
     return blank, [cv.contourArea(cnt) for cnt in contours]
 
 copyHandGel = createImg(i)
@@ -116,27 +117,56 @@ mHandNoGel, aHandNoGel = contour(mGrayBlurredHandNoGel, copyHandNoGel, (255, 255
 ### Gel ###
 
 imgHsvGel = cv.cvtColor(cv.bitwise_and(handGel, mHandGel), cv.COLOR_BGR2HSV)
-cv.imshow('hsv', imgHsvGel)
 hGel,sGel,vGel = cv.split(imgHsvGel)
 hsvGel = [hGel,sGel,vGel]
 imgHsvNoGel = cv.cvtColor(cv.bitwise_and(handNoGel, mHandNoGel), cv.COLOR_BGR2HSV)
 hNoGel,sNoGel,vNoGel = cv.split(cv.cvtColor(imgHsvNoGel, cv.COLOR_BGR2HSV))
 hsvNoGel = [hNoGel,sNoGel,vNoGel]
+# cv.imshow('hsv', np.hstack([imgHsvGel, imgHsvNoGel]))
 
 copyHandGel2 = createImg(i)
 copyHandNoGel2 = createImg(j)
 # hHsvGel = one_d_histogram(hsvGel, f'HSV Gel {i}', hide=False)
 # hHsvNoGel = one_d_histogram(hsvNoGel, f'HSV no Gel {i}', hide=False)
-hHGel = one_d_histogram(hsvGel[0], f'Hue Gel {i}', hide=True)
-sHGel = one_d_histogram(hsvGel[1], f'Sat Gel {i}', hide=False)
-hHNoGel = one_d_histogram(hsvNoGel[0], f'Hue no Gel {i}', hide=True)
-lower = np.array([100, 30, 0], dtype='uint8')
-upper = np.array([134, 160, 255], dtype='uint8')
-mGel = cv.inRange(imgHsvGel, lower, upper)
-mGel, aGel = contour(mGel, copyHandGel2, (255, 255, 255), i, 'Gel', hide=False)
-# mNoGel, aNoGel = contour(mGrayBlurredNoGel, copyNoGel, (255, 255, 255), j, 'gray blurred no gel', hide=True)
-# cv.imshow('ras', mGel)
+# hHGel = one_d_histogram(hsvGel[0], f'Hue Gel {i}', hide=True)
+# sHGel = one_d_histogram(hsvGel[1], f'Sat Gel {i}', hide=True)
+# vHGel = one_d_histogram(hsvGel[2], f'Val Gel {i}', hide=True)
+# hNoHGel = one_d_histogram(hsvNoGel[0], f'Hue no Gel {i}', hide=True)
+# sNoHGel = one_d_histogram(hsvNoGel[1], f'Sat no Gel {i}', hide=True)
+# vNoHGel = one_d_histogram(hsvNoGel[2], f'Val no Gel {i}', hide=True)
 
+def nothing(x):
+    pass
+
+cv.namedWindow(f'Gel {i}')
+cv.createTrackbar('Hue min', f'Gel {i}', 0, 255, nothing)
+cv.createTrackbar('Hue max', f'Gel {i}', 255, 255, nothing)
+cv.createTrackbar('Sat min', f'Gel {i}', 0, 255, nothing)
+cv.createTrackbar('Sat max', f'Gel {i}', 255, 255, nothing)
+cv.createTrackbar('Val min', f'Gel {i}', 0, 255, nothing)
+cv.createTrackbar('Val max', f'Gel {i}', 255, 255, nothing)
+
+h_min = 0
+s_min = 0
+v_min = 0
+h_max = 255
+s_max = 255
+v_max = 255
+while True:
+    img = handGel.copy()
+    lower = np.array([h_min, s_min, v_min], dtype='uint8')
+    upper = np.array([h_max, s_max, v_max], dtype='uint8')
+    mGel = cv.inRange(imgHsvGel, lower, upper)
+    mGel, aGel = contour(mGel, img, (255, 255, 255), i, f'Gel {i}', hide=True)
+    cv.imshow(f'Gel {i}', img)
+    if cv.waitKey(0) & 0xFF == 'q':
+        break
+    h_min = cv.getTrackbarPos('Hue min', f'Gel {i}')
+    h_max = cv.getTrackbarPos('Hue max', f'Gel {i}')
+    s_min = cv.getTrackbarPos('Sat min', f'Gel {i}')
+    s_max = cv.getTrackbarPos('Sat max', f'Gel {i}')
+    v_min = cv.getTrackbarPos('Val min', f'Gel {i}')
+    v_max = cv.getTrackbarPos('Val max', f'Gel {i}')
 
 plt.show()
 cv.waitKey(0)
