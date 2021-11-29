88a4a2aba07cefb56e3cf3d6d86cbe574fdb48be
Sres0 <susanarepo@gmail.com>
2021-11-29 09:14:15 -0500

    cam height & width automatized

--- Processing/dev/save.py
+++ Processing/dev/save.py
@@ -1,18 +1,15 @@
 import cv2 as cv
 
 cap = cv.VideoCapture(1, cv.CAP_DSHOW)
-cap.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(cv.CAP_PROP_FRAME_WIDTH, 1280/3)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, 720/3)
 
 def video():
     while True:
         _, frame = cap.read()
-        cv.line(frame, (0, int(720*4/5)), (1280, int(720*4/5)), (0, 0, 255), 2) # Línea de muñeca 1/5 de altura img
+        cv.line(frame, (0, int(cap.get(4)*4/5)), (cap.get(3), int(cap.get(4)*4/5)), (0, 0, 255), 2) # Línea de muñeca 1/5 de altura img
         cv.imshow('Capture', frame)
         if cv.waitKey(1) & 0xFF == ord('q'):
-            w = int(1280/3)
-            h = int(720/3)
-            frame = cv.resize(frame, (w,h))
             return frame
 
 i = input('>> ')
