620fa1a5251932208d5818a4bc2d1e3c0c9c1941
Sres0 <susanarepo@gmail.com>
2021-11-30 21:13:13 -0500

    back & palm w percentages & file try

--- Processing/dev/main.py
+++ Processing/dev/main.py
@@ -2,10 +2,10 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.signal as sig
+import errno
+import os
 
 results = []
-i = 634 # con gel
-j = i - 2
 
 def path(i): return f'Processing/Bank/Imgs/hand_test_{i}.png'
 
@@ -19,20 +19,6 @@ def createImg(i, hide=True, resized=False):
     if not hide: cv.imshow(f'Original {i}', img)
     return img
 
-handGel = createImg(i, hide=True)
-handNoGel = createImg(j, hide=True)
-
-### MASK ###
-
-medium_blur = (3,3)
-big_blur = (5,5)
-
-blurredHandGel = cv.blur(handGel, medium_blur)
-blurredHandNoGel = cv.blur(handNoGel, medium_blur)
-
-grayBlurredHandGel = cv.cvtColor(blurredHandGel, cv.COLOR_BGR2GRAY)
-grayBlurredHandNoGel = cv.cvtColor(blurredHandNoGel, cv.COLOR_BGR2GRAY)
-
 def set_histogram(title):
     plt.figure()
     plt.title(title)
@@ -43,7 +29,6 @@ def set_histogram(title):
 
 def one_d_histogram(img, title, hide=True, min=1, normalize=False):
     hist = []
-    
     if not hide: set_histogram(title)
     if type(img) == list:
         for cnl in img:
@@ -56,7 +41,6 @@ def one_d_histogram(img, title, hide=True, min=1, normalize=False):
         if normalize: cv.normalize(hist,hist,0,1,cv.NORM_MINMAX)
         if not hide: plt.plot(hist)
         hist = [y[0] for y in hist] # Retorna el histograma del último canal
-
     return hist
 
 def find_hand_thresh(hist, fs=60, hide=True, title='hand threshold'):
@@ -78,16 +62,10 @@ def find_hand_thresh(hist, fs=60, hide=True, title='hand threshold'):
         plt.plot(thresh,y[thresh], "x")
     return thresh
 
-hGrayBlurredHandGel = one_d_histogram(grayBlurredHandGel, 'Blurred gray gel', hide=True)
-hGrayBlurredHandNoGel = one_d_histogram(grayBlurredHandNoGel, 'Blurred gray no gel', hide=True)
-
 def get_binary_mask(i, thresh, img, channel_name, method=cv.THRESH_BINARY, hide=True):
     _, mask = cv.threshold(img, thresh, 255, method)
     if not hide: cv.imshow(f'Mascara {channel_name} {i} | {thresh}', mask)
     return mask
-
-mGrayBlurredHandGel = get_binary_mask(i, find_hand_thresh(hGrayBlurredHandGel, title='gel thresh', hide=True), grayBlurredHandGel, 'gray blurred gel')
-mGrayBlurredHandNoGel = get_binary_mask(i, find_hand_thresh(hGrayBlurredHandNoGel, title='no gel thresh', hide=True), grayBlurredHandNoGel, 'gray blurred no gel')
 
 def contour(mask, img, color, i, title, hide=True):
     blank = np.zeros((img.shape), dtype=np.uint8)
@@ -97,40 +75,19 @@ def contour(mask, img, color, i, title, hide=True):
         cv.drawContours(blank, [cnt], -1, color, -1)
         cv.drawContours(img, [cnt], -1, color, 1)
     if not hide:
-        cv.imshow(title+f' {i}', np.hstack([img,handNoGel]))
         results.append(img)
     areas = [cv.contourArea(cnt) for cnt in contours]
     return blank, sum(areas)
 
-copyHandGel = handGel.copy()
-copyHandNoGel = handNoGel.copy()
-mHandGel, aHandGel = contour(mGrayBlurredHandGel, copyHandGel, (255, 255, 255), i, 'gray blurred gel', hide=True)
-mHandNoGel, aHandNoGel = contour(mGrayBlurredHandNoGel, copyHandNoGel, (255, 255, 255), j, 'gray blurred no gel', hide=True)
-
-### GEL ###
-
-# HSV #
 def split_n_clahe(img):
     x,y,z = cv.split(img)
     clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(3,3))
-    for chan in (x,y,z):
-        chan = clahe.apply(chan)
+    for cnl in (x,y,z):
+        cnl = clahe.apply(cnl)
     img = cv.merge((x,y,z))
     return x,y,z,img
 
-imgHsvGel = cv.cvtColor(cv.bitwise_and(handGel, mHandGel), cv.COLOR_BGR2HSV)
-imgHsvNoGel = cv.cvtColor(cv.bitwise_and(handNoGel, mHandNoGel), cv.COLOR_BGR2HSV)
-
-claheGelH, claheGelS, claheGelV, claheGelHSV = split_n_clahe(imgHsvGel)
-claheNoGelH, claheNoGelS, claheNoGelV, claheNoGelHSV = split_n_clahe(imgHsvNoGel)
-
-hGelVsNoGel = [claheGelH,claheNoGelH]
-hHGelVsNoGel = one_d_histogram(hGelVsNoGel, f'Hue gel vs no gel | {i}', min=1, hide=True, normalize=True)
-
-imgHsvgel = cv.merge((claheGelH, claheGelS, claheGelV))
-imgHsvNoGel = cv.merge((claheNoGelH, claheNoGelS, claheNoGelV))
-
-def find_gel_percentage(hHue, hide=True, title='Presencia de gel'):
+def find_gel_percentage(hHue, handGel, hsvGel, aHandGel, hide=True, title='Presencia de gel'):
     hThreshMax = 254
     diff = []
     x0 = []
@@ -144,13 +101,11 @@ def find_gel_percentage(hHue, hide=True, title='Presencia de gel'):
     upper = np.array([hThreshMax, 255, 255], dtype='uint8')
     if hThreshMax != 254:
         copyHandGel = handGel.copy()
-        mGel = cv.inRange(imgHsvGel, lower, upper)
+        mGel = cv.inRange(hsvGel, lower, upper)
         mGel, aGel = contour(mGel, copyHandGel, (255, 255, 255), i, title, hide=False)
         percentage = round((aGel*100)/aHandGel,2)
-        print('Porcentaje lavado:',percentage,'%')
     else:
         percentage=0
-        print('Error: porcentaje de gel no identificado.')
     if not hide: 
         set_histogram(title)
         x = np.array(range(0,255))
@@ -159,7 +114,70 @@ def find_gel_percentage(hHue, hide=True, title='Presencia de gel'):
         plt.plot(hThreshMax,hHue[0][hThreshMax], "x")
     return percentage
 
-gelPercentage = find_gel_percentage(hHGelVsNoGel, hide=True)
 
-plt.show()
-cv.waitKey(0)
+w = int(1280/3)
+h = int(720/3)
+
+def text(img, percentage):
+    if percentage >= 80: color = (0,255,0)
+    else: color = (0,0,255)
+    if percentage == 0: text = 'Error: porcentaje de gel no identificado.'
+    else: text = str(percentage)+'%'
+    cv.putText(img, text, (int(w/3), int(h - h/4)), cv.FONT_HERSHEY_DUPLEX, 1, color)
+
+def show_gel_percentage(i):
+
+    j = i - 2
+
+    handGel = createImg(i, hide=True)
+    handNoGel = createImg(j, hide=True)
+
+    ### Hand mask ###
+    medium_blur = (3,3)
+    blurredHandGel = cv.blur(handGel, medium_blur)
+    blurredHandNoGel = cv.blur(handNoGel, medium_blur)
+    grayBlurredHandGel = cv.cvtColor(blurredHandGel, cv.COLOR_BGR2GRAY)
+    grayBlurredHandNoGel = cv.cvtColor(blurredHandNoGel, cv.COLOR_BGR2GRAY)
+    hGrayBlurredHandGel = one_d_histogram(grayBlurredHandGel, 'Blurred gray gel', hide=True)
+    hGrayBlurredHandNoGel = one_d_histogram(grayBlurredHandNoGel, 'Blurred gray no gel', hide=True)
+    mGrayBlurredHandGel = get_binary_mask(i, find_hand_thresh(hGrayBlurredHandGel, title='gel thresh', hide=True), grayBlurredHandGel, 'gray blurred gel')
+    mGrayBlurredHandNoGel = get_binary_mask(i, find_hand_thresh(hGrayBlurredHandNoGel, title='no gel thresh', hide=True), grayBlurredHandNoGel, 'gray blurred no gel')
+    copyHandGel = handGel.copy()
+    copyHandNoGel = handNoGel.copy()
+    mHandGel, aHandGel = contour(mGrayBlurredHandGel, copyHandGel, (255, 255, 255), i, 'gray blurred gel', hide=True)
+    mHandNoGel, aHandNoGel = contour(mGrayBlurredHandNoGel, copyHandNoGel, (255, 255, 255), j, 'gray blurred no gel', hide=True)
+
+    ### Gel mask ###
+    imgHsvGel = cv.cvtColor(cv.bitwise_and(handGel, mHandGel), cv.COLOR_BGR2HSV)
+    imgHsvNoGel = cv.cvtColor(cv.bitwise_and(handNoGel, mHandNoGel), cv.COLOR_BGR2HSV)
+
+    claheGelH, claheGelS, claheGelV, claheGelHSV = split_n_clahe(imgHsvGel)
+    claheNoGelH, claheNoGelS, claheNoGelV, claheNoGelHSV = split_n_clahe(imgHsvNoGel)
+
+    hGelVsNoGel = [claheGelH,claheNoGelH]
+    hHGelVsNoGel = one_d_histogram(hGelVsNoGel, f'Hue gel vs no gel | {i}', min=1, hide=True, normalize=True)
+
+    imgHsvGel = cv.merge((claheGelH, claheGelS, claheGelV))
+    gelPercentage = find_gel_percentage(hHGelVsNoGel, handGel, imgHsvGel, aHandGel, hide=True)
+    return gelPercentage
+
+while 1:
+    try: 
+        i = int(input('Ingrese número de imagen: '))
+        j = i + 1
+        for num in [i-2, i-1, i, i+1]:
+            if not os.path.exists(path(num)):
+                raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path(num))
+        back = show_gel_percentage(i)
+        palm = show_gel_percentage(j)
+        text(results[0],back)
+        text(results[1],palm)
+        cv.imshow('Dorso', results[0])
+        cv.imshow('Palma', results[1])
+        cv.waitKey(0)
+        break
+    except ValueError:
+        if input('Error: digite un valor adecuado. Presione "x" para salir\n>> ') == 'x': break
+    except FileNotFoundError:
+        if input('Error: digite un valor adecuado. Presione "x" para salir\n>> ') == 'x':
+            break
