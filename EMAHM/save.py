26b9eaca01bf22eb3343452c8bb27c01cba43785
Sres0 <susanarepo@gmail.com>
2021-11-30 21:33:50 -0500

    functional algorithm w sample bank id img

---
+++ EMAHM/save.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+
+h = int(720/2)
+w = int(1280/2)
+
+cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, h)
+cap.set(cv.CAP_PROP_FRAME_WIDTH, w)
+
+def video(h,w):
+    while True:
+        _, frame = cap.read()
+        cv.line(frame, (0, int(h*4/5)), (w, int(h*4/5)), (0, 0, 255), 2) # Línea de muñeca 1/5 de altura img
+        cv.imshow('Capture', frame)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            w = int(1280/3)
+            h = int(720/3)
+            frame = cv.resize(frame, (w,h))
+            return frame
+
+id = input('Identificación: ')
+
+for i in range(1,5):
+    cv.imwrite(r'EMAHM\Bank\\'+f'{id}_{i}.png', video(h,w))
+    print(f'Toma {i} guardada.')
+
+cap.release()
+cv.destroyAllWindows()
